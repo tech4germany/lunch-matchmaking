e8e62d797afbfd8e474f704b83942607fd24f485
Benjamin Degenhart <b.degenhart.work@gmail.com>
2020-08-11 19:46:13 +0200

    Store already_met_with in people

--- script.js
+++ script.js
@@ -12,21 +12,18 @@ while (line = liner.next()) {
         "in_team": Number(parts[0]),
         "id": id,
         "name": parts[2],
-        "discipline": parts[3].substr(0, parts[3].length - 1) // strip away new line character
+        "discipline": parts[3].substr(0, parts[3].length - 1), // strip away new line character
+        "already_met_with": []
     };
 }
-
-pairsAlreadyMet = [];
 
 liner = new readlines('lunch1.csv');
 while (line = liner.next()) {
     let parts = line.toString('utf8').split(',');
     let id1 = Number(parts[0]);
     let id2 = Number(parts[1]);
-    pairsAlreadyMet.push({
-        "id1": id1,
-        "id2": id2
-    });
+    people[id1].already_met_with.push(id2);
+    people[id2].already_met_with.push(id1);
 }
 
 console.log(pairsAlreadyMet);
