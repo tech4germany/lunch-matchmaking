8de77b8c627000017bd73384b8ab7f64a5d3aa38
Benjamin Degenhart <b.degenhart.work@gmail.com>
2020-08-11 16:46:01 +0200

    Use n-readlines instead

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-const lineReader = require('line-reader');
-const teams = require('./teams.json');
+const liner = new (require('n-readlines'))('people.csv');
+const teams = require('./teams.json').teams;
 
-console.log(teams);
+people = [];
+let line;
 
-lineReader.eachLine('people.csv', line => {
-    console.log(line);
-});
+while (line = liner.next()) {
+    let parts = line.toString('utf8').split(',');
+    people.push({
+        "in-team": parts[0],
+        "name": parts[1],
+        "discipline": parts[2].substr(0, parts[2].length - 1) // strip away new line character
+    });
+}
+
+console.log(people);
