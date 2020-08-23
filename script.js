ecf0e41e9dbaac89062113ea5a5ee48ebea4706e
Benjamin Degenhart <b.degenhart.work@gmail.com>
2020-08-23 19:20:33 +0200

    Include lunch2 to create lunch3

--- script.js
+++ script.js
@@ -39,6 +39,7 @@ const importBlockers = csvFile => {
 
 importBlockers('input/blockers/other.csv');
 importBlockers('input/blockers/lunch1.csv');
+importBlockers('input/blockers/lunch2.csv');
 
 const randomIntFromInterval = (min, max) => { // min and max included
     return Math.floor(Math.random() * (max - min + 1) + min);
@@ -83,7 +84,7 @@ for (let i = 0; i < pairs.length; i++) {
 }
 
 // write them out as next lunch.csv
-fs.writeFile('input/blockers/lunch2.csv', csvContent, err => {});
+fs.writeFile('input/blockers/lunch3.csv', csvContent, err => {});
 
 // TEST
 for (let i = 0; i < pairs.length; i++) {
