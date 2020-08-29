d3b8f9977440f6e37689ee7474142778603814dd
Benjamin Degenhart <b.degenhart.work@gmail.com>
2020-08-29 18:10:39 +0200

    Rename to ungroupedPeople and groups

--- script.js
+++ script.js
@@ -47,14 +47,14 @@ const randomIntFromInterval = (min, max) => { // min and max included
     return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-let unpairedPeopleIds = Object.keys(people);
-let pairs = [];
+let ungroupedPeople = Object.keys(people);
+let groups = [];
 
 for (let j = 0; j < 20; j ++) {
-    let p1id = unpairedPeopleIds[0];
+    let p1id = ungroupedPeople[0];
     let suitablePersons = [];
-    for (let i = 1; i < unpairedPeopleIds.length; i++) {
-        let p2IdToCheck = unpairedPeopleIds[i];
+    for (let i = 1; i < ungroupedPeople.length; i++) {
+        let p2IdToCheck = ungroupedPeople[i];
         let p1 = people[p1id];
         if (p1.already_met_with.includes(p2IdToCheck)) {
             continue;
@@ -69,18 +69,18 @@ for (let j = 0; j < 20; j ++) {
 
     // TODO make the loop better so that these undefined-checks are not necessary
     if (p1id !== undefined && p2id !== undefined) {
-        pairs.push([p1id, p2id]);
+        groups.push([p1id, p2id]);
     }
 
-    unpairedPeopleIds.splice(unpairedPeopleIds.indexOf(p2id), 1);
-    unpairedPeopleIds.splice(0, 1);
+    ungroupedPeople.splice(ungroupedPeople.indexOf(p2id), 1);
+    ungroupedPeople.splice(0, 1);
 }
 
 // pretty print matches
 let csvContent = '';
-for (let i = 0; i < pairs.length; i++) {
-    let id1 = pairs[i][0];
-    let id2 = pairs[i][1];
+for (let i = 0; i < groups.length; i++) {
+    let id1 = groups[i][0];
+    let id2 = groups[i][1];
     console.log(people[id1].name + " & " + people[id2].name);
     csvContent += id1 + ',' + id2 + '\n';
 }
@@ -91,9 +91,9 @@ fs.writeFile('input/blockers/lunch4.csv', csvContent, err => {});
 
 // TESTS ---------------------------------------------------------
 
-for (let i = 0; i < pairs.length; i++) {
-    let p1 = people[pairs[i][0]];
-    let p2 = people[pairs[i][1]];
+for (let i = 0; i < groups.length; i++) {
+    let p1 = people[groups[i][0]];
+    let p2 = people[groups[i][1]];
     if (p1.in_team === p2.in_team || p1.already_met_with.includes(p2.id) || p1.already_met_with.includes(p2.id)) {
         console.log("ERROR: ", p1.id, p2.id);
     }
